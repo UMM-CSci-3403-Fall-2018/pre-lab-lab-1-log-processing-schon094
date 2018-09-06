53a95f463f163d2c5c36efc4ceffc1fa7731bf4b
schon094 <35474566+schon094@users.noreply.github.com>
2018-09-05 19:21:37 -0500

    Add files via upload

---
+++ wrap_contents.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#file containing constants to be wrapped
+file=$1
+#name of specified header and footer(*_footer.html, *_header.html)
+bits=$2
+#name of resulting file
+result=$3
+
+cat ${bits}_header.html $file ${bits}_footer.html > $resulit
